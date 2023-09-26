2575930627dfe47975dc623d5c1cce166a5715ae
201360 <Nagakeerthana.Kollavajjala@amdocs.com>
2023-09-26 11:59:02 +0530

    commit java

---
+++ src/test/java/com/amdocs/demo890/Demo890ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdocs.demo890;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo890ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
